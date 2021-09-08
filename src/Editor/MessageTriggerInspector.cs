65d79b26f07ce06adb917eb32fff7a45b64d3008
GF47 <liougourenmo@gmail.com>
2021-09-08 11:01:20 +0800

    add unity extensions

---
+++ src/Editor/MessageTriggerInspector.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GMessage.Editor.Inspectors
+{
+    [CustomEditor(typeof(MessageTrigger))]
+    public class MessageTriggerInspector : UnityEditor.Editor
+    {
+        private int[] _moduleIDArray;
+        private string[] _moduleNameArray;
+
+        private int[] _messageIDArray;
+        private string[] _messageNameArray;
+
+        private void OnEnable()
+        {
+            var fields = typeof(ModuleID).GetFields(BindingFlags.Public | BindingFlags.Static);
+            fields = Array.FindAll(fields, f => f.FieldType == typeof(int));
+
+            _moduleIDArray = new int[fields.Length];
+            _moduleNameArray = new string[fields.Length + 1];
+
+            _moduleNameArray[0] = "Global";
+            for (int i = 0; i < fields.Length; i++)
+            {
+                _moduleIDArray[i] = (int)fields[i].GetRawConstantValue();
+                _moduleNameArray[i + 1] = $"[ {fields[i].Name} ] {fields[i].GetCustomAttribute<DescriptionAttribute>()?.Description}";
+            }
+
+            fields = typeof(DefinedID).GetFields(BindingFlags.Public | BindingFlags.Static);
+            fields = Array.FindAll(fields, f => f.FieldType == typeof(int));
+
+            _messageIDArray = new int[fields.Length];
+            _messageNameArray = new string[fields.Length + 1];
+
+            _messageNameArray[0] = "None";
+            for (int i = 0; i < fields.Length; i++)
+            {
+                _messageIDArray[i] = (int)fields[i].GetRawConstantValue();
+                _messageNameArray[i + 1] = $"{fields[i].Name} <---> {fields[i].GetCustomAttribute<DescriptionAttribute>()?.Description}";
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var trigger = target as MessageTrigger;
+            var flag = Array.IndexOf(_moduleIDArray, trigger.moduleID);
+
+            flag = EditorGUILayout.Popup("Modules", flag + 1, _moduleNameArray);
+            trigger.moduleID = flag != 0 ? _moduleIDArray[flag - 1] : Dispatcher.GLOBAL;
+
+            EditorGUILayout.BeginVertical();
+
+            for (int i = 0; i < trigger.Triggers.Count; i++)
+            {
+                var msg = trigger.Triggers[i];
+
+                EditorGUILayout.BeginHorizontal();
+
+                msg.type = (EventTriggerType)EditorGUILayout.EnumPopup(msg.type);
+
+                flag = Array.IndexOf(_messageIDArray, msg.id);
+                flag = EditorGUILayout.Popup(flag + 1, _messageNameArray);
+                msg.id = flag != 0 ? _messageIDArray[flag - 1] : 0;
+
+                msg.content = EditorGUILayout.TextField(msg.content);
+
+                if (!msg.Equals(trigger.Triggers[i]))
+                {
+                    Undo.RecordObject(trigger, "change trigger");
+                    trigger.Triggers[i] = msg;
+                    EditorUtility.SetDirty(trigger);
+                }
+
+                if (GUILayout.Button("-", EditorStyles.miniButton))
+                {
+                    Undo.RecordObject(trigger, "remove trigger");
+                    trigger.Triggers.RemoveAt(i);
+                    EditorUtility.SetDirty(trigger);
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if (GUILayout.Button("+"))
+            {
+                Undo.RecordObject(trigger, "add trigger");
+                trigger.Triggers.Add(new MessageTrigger.MessageData());
+                EditorUtility.SetDirty(trigger);
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
---
+++ src/Extension/MessageTrigger.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GMessage
+{
+    public class MessageTrigger : MonoBehaviour,
+        IPointerEnterHandler,
+        IPointerExitHandler,
+        IPointerDownHandler,
+        IPointerUpHandler,
+        IPointerClickHandler,
+        ISelectHandler,
+        IDeselectHandler,
+        IUpdateSelectedHandler,
+        ISubmitHandler,
+        ICancelHandler
+    {
+
+        public int moduleID = Dispatcher.GLOBAL;
+
+        [Serializable]
+        public struct MessageData : IEquatable<MessageData>
+        {
+            public EventTriggerType type;
+            public int id;
+            public string content;
+            public Func<object> commonContent;
+
+            public bool Equals(MessageData b)
+            {
+                return type == b.type && id == b.id && content == b.content && commonContent == b.commonContent;
+            }
+        }
+        public List<MessageData> Triggers => _triggers;
+        [SerializeField]
+        private List<MessageData> _triggers = new List<MessageData>();
+
+        private Dispatcher GetDispatcher()
+        {
+            return Dispatcher.Instance<Dispatcher>(moduleID);
+        }
+
+        #region 响应方法
+
+        private void Execute(EventTriggerType type)
+        {
+            for (int i = 0; i < _triggers.Count; i++)
+            {
+                if (_triggers[i].type == type)
+                {
+                    new Message(_triggers[i].id, this,
+                                _triggers[i].commonContent == null ? _triggers[i].content : _triggers[i].commonContent()
+                    ).DispatchTo(GetDispatcher());
+                }
+            }
+        }
+
+        public void OnCancel(BaseEventData eventData)
+        {
+            Execute(EventTriggerType.Cancel);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            Execute(EventTriggerType.Deselect);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Execute(EventTriggerType.PointerClick);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Execute(EventTriggerType.PointerDown);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Execute(EventTriggerType.PointerEnter);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Execute(EventTriggerType.PointerExit);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Execute(EventTriggerType.PointerUp);
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            Execute(EventTriggerType.Select);
+        }
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            Execute(EventTriggerType.Submit);
+        }
+
+        public void OnUpdateSelected(BaseEventData eventData)
+        {
+            Execute(EventTriggerType.UpdateSelected);
+        }
+
+        #endregion
+    }
+}
